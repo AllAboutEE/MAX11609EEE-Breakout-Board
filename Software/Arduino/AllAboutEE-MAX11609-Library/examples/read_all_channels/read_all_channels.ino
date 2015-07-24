1895e788a7d10c1643f36a94b1a21e91664f4be7
AllAboutEE <miguel@allaboutee.com>
2015-07-24 06:31:13 -0700

    Added and removed comments.

--- Software/Arduino/AllAboutEE-MAX11609-Library/examples/read_all_channels/read_all_channels.ino
+++ Software/Arduino/AllAboutEE-MAX11609-Library/examples/read_all_channels/read_all_channels.ino
@@ -14,8 +14,7 @@ MAX11609 converter;
 void setup()
 {
     Serial.begin(9600);
-    converter.begin(0,2,MAX11609::REF_VDD); // For ESP8266 Arduino SDK. Use pins 0 for SDA, use pin 2 for SCL. VDD is reference (i.e. VDD = 1023)
-    // converter.begin(MAX11609::REF_VDD); // Use this line instead of previous if you want to use Arduino the arduino instead of ESP8266. VDD is reference (i.e. VDD = 1023)
+    converter.begin(0,2,MAX11609::REF_VDD); // For ESP8266 Arduino SDK. Use pins GPIO0 for SDA, use pin GPIO2 for SCL. VDD (3.3V) is reference (i.e. VDD = 3.3V = 1023)
 }
 
 void loop()
@@ -24,7 +23,7 @@ void loop()
 
     converter.scan(result); // The conversion results are stored in the "result" array.
 
-    // display results
+    // display results from all 8 channels
     for(int i=0;i<8;i++)
     {
         Serial.print("Channel ");
--- Software/Arduino/AllAboutEE-MAX11609-Library/examples/template/template.ino
+++ Software/Arduino/AllAboutEE-MAX11609-Library/examples/template/template.ino
@@ -7,8 +7,7 @@ MAX11609 converter;
 
 void setup()
 {
-    converter.begin(0,2,MAX11609::REF_VDD); // For ESP8266 Arduino SDK. Use pins 0 for SDA, use pin 2 for SCL. VDD is reference (i.e. VDD = 1023)
-    // converter.begin(MAX11609::REF_VDD); // Use this line instead of previous if you want to use Arduino the arduino instead of ESP8266. VDD is reference (i.e. VDD = 1023)
+    converter.begin(0,2,MAX11609::REF_VDD); // For ESP8266 Arduino SDK. Use pins GPIO0 for SDA, use pin GPIO2 for SCL. VDD (3.3V) is reference (i.e. VDD = 3.3V = 1023)
 }
 
 void loop()
